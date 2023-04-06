28fda8acba3af4e2a73a433e9886de36f018da01
Vaidik Kapoor <kapoor.vaidik@gmail.com>
2023-04-07 01:35:31 +0530

    add a prompt to continue copy with reset option

--- src/copy.ts
+++ src/copy.ts
@@ -1,8 +1,13 @@
 import {Command} from 'commander';
 import colors from 'colors';
+import inquirer from 'inquirer';
 import {list} from './list';
 
 colors.enable();
+
+async function copy(sourceAccountId: string, sourceContainerId: string, targetAccountId: string, targetContainerId: string) {
+  console.log('Copied entities succsessfully'.green);
+}
 
 const copy_cmd = new Command('copy');
 copy_cmd
@@ -30,8 +35,36 @@ copy_cmd
 copy_cmd.action(async () => {
   const sourceAccountId: string = copy_cmd.opts().sourceAccount;
   const sourceContainerId: string = copy_cmd.opts().sourceContainer;
+  const targetAccountId: string = copy_cmd.opts().targetAccount;
+  const targetContainerId: string = copy_cmd.opts().targetContainer;
+  const isReset: boolean = copy_cmd.opts().reset;
 
-  await list(sourceAccountId, sourceContainerId);
+  if (isReset) {
+    await list(sourceAccountId, sourceContainerId);
+    inquirer
+      .prompt([
+        {
+          type: 'confirm',
+          name: 'continueReset',
+          message: 'Do you want to continue to reset the target GTM account and copy all entities from the source GTM account?',
+          default: false,
+        }
+      ])
+      .then(async (answers) => {
+        if (answers.continueReset) {
+          console.log('Resetting target GTM account and copying entities from source GTM account...'.gray);
+          await copy(sourceAccountId, sourceContainerId, targetAccountId, targetContainerId);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+        // if (error.isTtyError) {
+        //   // Prompt couldn't be rendered in the current environment
+        // } else {
+        //   // Something else went wrong
+        // }
+      });
+  }
 });
 
 export {copy_cmd};
