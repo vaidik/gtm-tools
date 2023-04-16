094e7f37f1cfc52a7cbbe39b1a729b3a77152a79
Vaidik Kapoor <kapoor.vaidik@gmail.com>
2023-04-16 19:42:56 +0530

    implement config manager

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,44 @@
+import path from 'path';
+import util from 'util';
+import {
+  validate
+} from 'class-validator';
+import { plainToClass } from 'class-transformer';
 import {Command} from 'commander';
 import {copy_cmd} from './copy.js';
 import {list_cmd} from './list.js';
+import {Config} from './config.js';
+import fs from 'fs';
 
+// Create a CLI command
 const cli = new Command();
+cli.name('gtm-tools');
+cli.option('--config <CONFIG_FILE>', "Path to the config file");
+
+// Register sub-commands
 cli.addCommand(list_cmd);
 cli.addCommand(copy_cmd);
+
+// Main command
+cli.hook('preAction', async (thisCommand, actionCommand) => {
+  const configRaw = JSON.parse(fs.readFileSync(path.join(process.cwd(), cli.opts().config), {encoding:'utf8', flag:'r'}));
+  const configObj = plainToClass(Config, configRaw as Object);
+
+  await validate(configObj, { forbidUnknownValues: false }).then(errors => {
+    if (errors.length > 0) {
+      console.log('Invalid configuration file. Validation failed.'.red);
+      console.log(util.inspect(errors, false, null, true));
+      process.exit(1);
+    }
+  });
+});
 
 async function main() {
   if (!process.env.GOOGLE_APPLICATION_CREDENTIALS) {
     console.log('GOOGLE_APPLICATION_CREDENTIALS environment variable must be set and pointing to a valid Google API credentials.json file.'.red)
     process.exit(1);
   }
-  cli.parse(process.argv);
+  await cli.parseAsync();
 }
 
 main().catch(e => {
--- src/list.ts
+++ src/list.ts
@@ -1,7 +1,8 @@
-import {Command} from 'commander';
+import {Command, Option} from 'commander';
 import Table from 'cli-table';
 import colors from 'colors';
 import {TagManagerData} from './core.js';
+import {Config} from './config.js';
 
 colors.enable();
 
@@ -37,14 +38,24 @@ async function list(account: TagManagerData) {
 
 const list_cmd = new Command('list');
 list_cmd
-  .requiredOption('-a, --account <ACCOUNT_ID>', "GTM account's Account ID")
-  .requiredOption('-c, --container <CONTAINER_ID>', "GTM account's Container ID")
-  .requiredOption('-w, --workspace <WORKSPACE_ID>', "GTM account's Workspace ID");
+  .option('-aa, --account-alias <ACCOUNT_ALIAS>', "GTM account's alias as specified in the config")
+  .addOption(new Option('-a, --account <ACCOUNT_ID>', "GTM account's Account ID").conflicts('accountAlias'))
+  .addOption(new Option('-c, --container <CONTAINER_ID>', "GTM account's Container ID").conflicts('accountAlias'))
+  .addOption(new Option('-w, --workspace <WORKSPACE_ID>', "GTM account's Workspace ID").conflicts('accountAlias'));
 
 list_cmd.action(async () => {
-  const accountId: string = list_cmd.opts().account;
-  const containerId: string = list_cmd.opts().container;
-  const workspaceId: string = list_cmd.opts().workspace;
+  const accountAlias: string = list_cmd.opts().accountAlias;
+  let accountId: string = list_cmd.opts().account;
+  let containerId: string = list_cmd.opts().container;
+  let workspaceId: string = list_cmd.opts().workspace;
+  if (accountAlias !== undefined) {
+    const config = new Config();
+    const accountConfig = config.getAccount(accountAlias);
+    accountId = accountConfig?.accountId as string;
+    containerId = accountConfig?.containerId as string;
+    workspaceId = accountConfig?.workspaceId as string;
+  }
+
   const account: TagManagerData = new TagManagerData(accountId, containerId, workspaceId);
   await account.init();
 
