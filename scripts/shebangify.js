872cd526d10971413203ed4779d57a20ec7e1369
Vaidik Kapoor <kapoor.vaidik@gmail.com>
2024-01-26 02:17:49 +0530

    chore: add release pipeline to publish to npm registry (#18)

--- scripts/shebangify.js
+++ scripts/shebangify.js
@@ -1,6 +1,5 @@
-#!/usr/bin/env node
 import fs from 'fs';
-var path = process.argv[2];
-var data = "#!/usr/bin/env node\n\n";
+const path = process.argv[2];
+let data = '#!/usr/bin/env node\n\n';
 data += fs.readFileSync(path);
 fs.writeFileSync(path, data);
